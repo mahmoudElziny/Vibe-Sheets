433507a3c508af26289246c777b65b681b4add9a
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-08-24 00:51:19 +0300

    logo implementation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { supabase } from "../lib/supabase";
+import logo from "../assets/logo2.png";
 
 export default function Navbar() {
   const navigate = useNavigate();
@@ -18,9 +19,9 @@ export default function Navbar() {
     <nav className="bg-[#4caf50] text-white p-4">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <div className="flex items-center space-x-4">
-          <Link to="/" className="hover:underline">
-            Home
-          </Link>
+          <div className="p-2 rounded-4xl">
+            <img src={logo} alt="Logo" className="h-14 w-14 rounded-lg" />
+          </div>
         </div>
 
         {!hideLogout && (
