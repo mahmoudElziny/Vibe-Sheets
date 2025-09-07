ff16cdcf494c95725b1bd20a66732225c0daf452
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-09-08 00:04:27 +0300

    tracking no & project tab title

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -2,8 +2,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { supabase } from "../lib/supabase";
-import { FaEdit, FaTrash, FaPlus, FaSave, FaArrowLeft } from 'react-icons/fa';
-
+import { FaEdit, FaTrash, FaPlus, FaSave, FaArrowLeft } from "react-icons/fa";
 
 export default function TablePage() {
   const { tableName } = useParams();
@@ -78,7 +77,7 @@ export default function TablePage() {
         const cleanRow = {};
         columns.forEach((col) => {
           if (
-            col !== "tracking_no" &&
+            // col !== "tracking_no" &&
             row[col] !== undefined &&
             row[col] !== ""
           ) {
@@ -105,9 +104,9 @@ export default function TablePage() {
     for (const row of existingRows) {
       const payload = {};
       columns.forEach((col) => {
-        if (col !== "tracking_no") {
-          payload[col] = row[col];
-        }
+        // if (col !== "tracking_no") {
+        payload[col] = row[col];
+        // }
       });
 
       const { error } = await supabase
@@ -143,7 +142,7 @@ export default function TablePage() {
 
   if (loading) return <p>Loading...</p>;
 
-return (
+  return (
     <div className="p-2">
       <button
         onClick={() => navigate("/")}
@@ -176,10 +175,10 @@ return (
                     key={col}
                     className="px-3 py-2 border text-xs text-left break-words whitespace-normal"
                     style={{
-                      maxWidth: col === "tracking_no" ? "60px" : "200px",
+                      maxWidth: col === "tracking_no" ? "80px" : "200px",
                       minWidth:
                         col === "tracking_no"
-                          ? "60px"
+                          ? "80px"
                           : col === "company_name"
                           ? "180px"
                           : "120px",
@@ -196,34 +195,38 @@ return (
                 <tr key={row.id || idx}>
                   {columns.map((col) => (
                     <td key={col} className="p-2 border bg-amber-50">
-                      {col === "tracking_no" ? (
+                      {
+                        /* {col === "tracking_no" ? (
                         <input
                           type="text"
                           value={row[col] || ""}
-                          disabled
+                          // disabled
+                          disabled={row.id && editingRow !== row.id}
                           className="w-full border px-3 py-2 rounded bg-gray-100 text-gray-500 text-sm"
                         />
-                      ) : col.toLowerCase().includes("date") ? (
-                        <input
-                          type="date"
-                          value={row[col] || ""}
-                          onChange={(e) =>
-                            handleChange(idx, col, e.target.value)
-                          }
-                          disabled={row.id && editingRow !== row.id}
-                          className="w-full border px-3 py-2 rounded text-xs"
-                        />
-                      ) : (
-                        <input
-                          type="text"
-                          value={row[col] || ""}
-                          onChange={(e) =>
-                            handleChange(idx, col, e.target.value)
-                          }
-                          disabled={row.id && editingRow !== row.id}
-                          className="w-full border px-3 py-2 rounded text-xs"
-                        />
-                      )}
+                      ) : */
+                        col.toLowerCase().includes("date") ? (
+                          <input
+                            type="date"
+                            value={row[col] || ""}
+                            onChange={(e) =>
+                              handleChange(idx, col, e.target.value)
+                            }
+                            disabled={row.id && editingRow !== row.id}
+                            className="w-full border px-3 py-2 rounded text-xs"
+                          />
+                        ) : (
+                          <input
+                            type="text"
+                            value={row[col] || ""}
+                            onChange={(e) =>
+                              handleChange(idx, col, e.target.value)
+                            }
+                            disabled={row.id && editingRow !== row.id}
+                            className="w-full border px-3 py-2 rounded text-xs"
+                          />
+                        )
+                      }
                     </td>
                   ))}
                   <td className="p-2 border flex gap-2">
